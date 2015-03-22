4487c40c5a3c9942cae044a23b93e6399bb74563
Pedro Teixeira <pedrofvteixeira@gmail.com>
2015-03-22 11:38:26 +0000

    added 'unread count' marker on top of the logged-in user

---
+++ static/custom/js/notifications-count-startup.js
@@ -0,0 +1,47 @@
+$(window).load(function(){
+
+  //callback to open each tab after the PerspectivesLoadedEvent gets fired
+  var callback = function(args) {
+    setTimeout(function(){
+      new NotificationsCount().check();
+
+    }, 1000);
+  };
+
+  var getPUCMethod = function( methodName, frame ) {
+    if ( frame === undefined ) {
+      return undefined; 
+    }
+
+    // if this frame has the method then assume this is the PUC frame
+    if ( frame[methodName] !== undefined ) {
+      return frame[methodName];
+    }
+
+    // if reached topmost frame and did not find method
+    if ( frame.parent === frame ) {
+      return undefined;
+    }
+    // else search ancestors
+    else {
+      return getPUCMethod( methodName, frame.parent );
+    }
+  };
+
+  //add the handler as soon as it becomes available
+  var functionTimed = function(){
+    if(typeof mantle_openTab != "undefined"){
+      window.clearInterval(timeoutManageOpenTab);
+      callback();
+    } else {
+      if(typeof mantle_addHandler != "undefined"){ 
+        window.top.mantle_addHandler("PerspectivesLoadedEvent", callback);
+        window.clearInterval(timeoutManageOpenTab);
+      } else {
+        //set runs interval again
+      }
+    }
+  };
+
+  var timeoutManageOpenTab = setInterval(functionTimed, 100);
+});
